a06ca041b8591f693e37c7bfd20d26312bb560bd
Nitish <nitish.m.d@gmail.com>
2014-05-07 01:25:50 -0400

    neater cleaner elegant

--- MineField.java
+++ MineField.java
@@ -2,6 +2,8 @@
  * Author: Nitish Devadiga
  * Description: TopCoder SRM 169 Div 1 250
  */
+import java.util.*;
+import static java.lang.Integer.parseInt;
 
 public class MineField {
 
@@ -15,56 +17,42 @@ public class MineField {
   public String[] getMineField(String mineLocations) {
     int[][] mineField = new int[9][9];
     String[] outputField = new String[9];
+    StringTokenizer st = new StringTokenizer(mineLocations, "(,)");
+    int[] dx = {-1,1,0,0,-1,1,1,-1};
+    int[] dy = {0,0,-1,1,-1,1,-1,1};
 
-
-    for(int i = 1; i < mineLocations.length(); ) {
-      //System.out.println("Debug: Locations"+mineLocations.charAt(i)+" "+mineLocations.charAt(i+2));
-      int x = Character.getNumericValue(mineLocations.charAt(i));
-      int y = Character.getNumericValue(mineLocations.charAt(i+2));
-
-      //System.out.println("Debug: "+x+" "+y);
+    while(st.hasMoreTokens()) {
+      int x = parseInt(st.nextToken());
+      int y = parseInt(st.nextToken());
 
       mineField[x][y] = -1;
 
-      // Mark borders
-      if(checkBorder(x-1,y))
-        mineField[x-1][y] = mineField[x-1][y] == -1? -1 : mineField[x-1][y] + 1;
-      if(checkBorder(x+1,y))
-        mineField[x+1][y] = mineField[x+1][y] == -1? -1 : mineField[x+1][y] + 1;
-      if(checkBorder(x,y-1))
-        mineField[x][y-1] = mineField[x][y-1] == -1? -1 : mineField[x][y-1] + 1;
-      if(checkBorder(x,y+1))
-        mineField[x][y+1] = mineField[x][y+1] == -1? -1 : mineField[x][y+1] + 1;
-      if(checkBorder(x-1,y-1))
-        mineField[x-1][y-1] = mineField[x-1][y-1] == -1? -1 :  mineField[x-1][y-1] + 1;
-      if(checkBorder(x+1,y+1))
-        mineField[x+1][y+1] = mineField[x+1][y+1] == -1? -1 : mineField[x+1][y+1] + 1;
-      if(checkBorder(x+1,y-1))
-        mineField[x+1][y-1] = mineField[x+1][y-1] == -1? -1 : mineField[x+1][y-1] + 1;
-      if(checkBorder(x-1,y+1))
-        mineField[x-1][y+1] = mineField[x-1][y+1] == -1? -1 :  mineField[x-1][y+1] + 1;
-
-      i = i + 5;
+      for(int i = 0; i < dx.length; i++) {
+        int ix = x + dx[i];
+        int iy = y + dy[i];
+        if(!checkBorder(ix, iy))
+          continue;
+        mineField[ix][iy] = mineField[ix][iy] == -1? -1 : mineField[ix][iy] + 1;
+      }
     }
-
 
     StringBuilder mines = new StringBuilder();
     String mine;
-    for(int i = 0; i < 9; i++) {
-      for(int j = 0; j < 9; j++) {
+    for(int i = 0; i < mineField.length; i++) {
+      for(int j = 0; j < mineField[i].length; j++) {
         mine = mineField[i][j] == -1? "M" : Integer.toString(mineField[i][j]);
         mines.append(mine);
       }
       outputField[i] = mines.toString();
-      //System.out.println(outputField[i]);
+      System.out.println(outputField[i]);
       mines.setLength(0);
     }
     return outputField;
   }
 
-  /*public static void main(String[] args) {
+  public static void main(String[] args) {
     String s = "(0,0)(1,0)(2,0)(3,0)(4,0)";
     MineField m = new MineField();
     m.getMineField(s);
-  }*/
+  }
 }
