0e8d2a743e114ecddf93e4e2c5b2959d7c5c3b49
Nitish <nitish.m.d@gmail.com>
2014-05-06 23:32:19 -0400

    updated minefield

--- MineField.java
+++ MineField.java
@@ -1,4 +1,11 @@
 public class MineField {
+
+  boolean checkBorder(int x, int y) {
+    if(x >= 0 && x <= 8 && y >= 0 && y <= 8) {
+      return true;
+    }
+    return false;
+  }
   
   public void getMineField(String mineLocations) {
     int[][] mineField = new int[9][9];
@@ -13,32 +20,38 @@ public class MineField {
 
       mineField[x][y] = -1;
 
-      // Here is the bug
-      if((x-1) >= 0 && (x+1) <= 8 && (y-1) >= 0 && (y+1) <= 8) {
-        // Mark borders
+      // Mark borders
+      if(checkBorder(x-1,y))
         mineField[x-1][y] = mineField[x-1][y] == -1? -1 : mineField[x-1][y] + 1;
-        System.out.println("Debug: "+mineField[x-1][y]);
+      if(checkBorder(x+1,y))
         mineField[x+1][y] = mineField[x+1][y] == -1? -1 : mineField[x+1][y] + 1;
+      if(checkBorder(x,y-1))
         mineField[x][y-1] = mineField[x][y-1] == -1? -1 : mineField[x][y-1] + 1;
+      if(checkBorder(x,y+1))
         mineField[x][y+1] = mineField[x][y+1] == -1? -1 : mineField[x][y+1] + 1;
+      if(checkBorder(x-1,y-1))
         mineField[x-1][y-1] = mineField[x-1][y-1] == -1? -1 :  mineField[x-1][y-1] + 1;
+      if(checkBorder(x+1,y+1))
         mineField[x+1][y+1] = mineField[x+1][y+1] == -1? -1 : mineField[x+1][y+1] + 1;
+      if(checkBorder(x+1,y-1))
         mineField[x+1][y-1] = mineField[x+1][y-1] == -1? -1 : mineField[x+1][y-1] + 1;
+      if(checkBorder(x-1,y+1))
         mineField[x-1][y+1] = mineField[x-1][y+1] == -1? -1 :  mineField[x-1][y+1] + 1;
-      }
+
       i = i + 5;
     }
 
-    for(int i = 0; i < 9; i++) {
+    // FOr testing purposes only
+    /*for(int i = 0; i < 9; i++) {
       for(int j = 0; j < 9; j++) {
         System.out.print(mineField[i][j]+" ");
       }
       System.out.println("");
-    }
+    }*/
   }
 
   public static void main(String[] args) {
-    String s = "(0,0)(1,0)(2,0)(3,0)(4,0)";
+    //String s = "(0,0)(1,0)(2,0)(3,0)(4,0)";
     MineField m = new MineField();
     m.getMineField(s);
   }
